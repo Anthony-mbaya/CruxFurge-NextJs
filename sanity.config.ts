20fad810a3593ae2095ede3ff553e8843ffc05b0
Anthony <anthonymwakulila@gmail.com>
2025-02-23 13:26:31 +0300

    onfigured for vercel deployment

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,9 @@
-//'use server'
+'use client'
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
-/*
+
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
@@ -28,24 +28,4 @@ export default defineConfig({
     markdownSchema()
   ],
 })
-*/
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import { markdownSchema } from "sanity-plugin-markdown";
-
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schemaTypes";
-import { structure } from "./sanity/structure";
-
-export default defineConfig({
-  basePath: "/studio",
-  projectId,
-  dataset,
-  schema,
-  plugins: [
-    structureTool({ structure }),
-    visionTool({ defaultApiVersion: apiVersion }),
-    markdownSchema(),
-  ],
-});
+ 
