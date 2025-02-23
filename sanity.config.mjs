55ed035fa620d6434ac9ac1ae9c2789a7dc107d5
Anthony <anthonymwakulila@gmail.com>
2025-02-23 12:06:31 +0300

    removed 'use client' from sanity.config.ts

--- sanity.config.mjs
+++ sanity.config.mjs
@@ -1,9 +1,9 @@
-//'use client'
+//'use server'
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
-
+/*
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
@@ -28,3 +28,24 @@ export default defineConfig({
     markdownSchema()
   ],
 })
+*/
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { markdownSchema } from "sanity-plugin-markdown";
+
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schemaTypes";
+import { structure } from "./sanity/structure";
+
+export default defineConfig({
+  basePath: "/studio",
+  projectId,
+  dataset,
+  schema,
+  plugins: [
+    structureTool({ structure }),
+    visionTool({ defaultApiVersion: apiVersion }),
+    markdownSchema(),
+  ],
+});
